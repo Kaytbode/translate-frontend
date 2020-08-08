c4d4950270931145d97401f5f71c6617bd3e22b1
kaytbode <bkaoluborode@gmail.com>
2020-08-08 19:53:36 +0100

    feat: User can find phrase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { 
+      value: '',
+      yorExp: '',
+      yorInt: '',
+      yorSpo: '',
+      yorVid: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({
+      value: event.target.value
+    });
+    event.preventDefault();
+  }
+
+  handleSubmit(event){
+    const uri = `http://localhost:5000/phrase/search/${this.state.value}`
+  
+    axios.get(uri)
+      .then(res => {
+        const { data } = res.data;
+        console.log(data);
+        this.setState({
+          yorExp: data[0].yor_explanation,
+          yorInt: data[0].yor_intonation,
+          yorSpo: data[0].yor_spoken,
+          yorVid: data[0].yor_video,
+        });
+        console.log(res.data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <div>
+        <form onSubmit = { this.handleSubmit }>
+          <input name='search_phrase' type='text' value={ this.state.value } onChange={ this.handleChange }/>
+          <button type='submit'>search</button>
+        </form>
+        <iframe
+          frameBorder="0"
+          width="400"
+          height="200"
+          src={this.state.yorExp}>
+        </iframe>
+        <iframe
+          frameBorder="0"
+          width="400"
+          height="200"
+          src={this.state.yorInt}>
+        </iframe>
+        <iframe
+          frameBorder="0"
+          width="400"
+          height="200"
+          src={this.state.yorSpo}>
+        </iframe>
+        <iframe
+          frameBorder="0"
+          width="400"
+          height="200"
+          src={this.state.yorVid}>
+        </iframe>
+      </div>
+      
+    );
+  }
 }
+
 
 export default App;
